c5b89cea05a5d778ab7f172e6dc7f5af30b00358
ArnaudRolin <63839312+ArnaudRolin@users.noreply.github.com>
2020-04-22 15:50:19 +0200

    maj lobby, raffraichissement automatique des séléctions,

--- ClashDefense/src/Arnaud/Lobby.java
+++ ClashDefense/src/Arnaud/Lobby.java
@@ -2,9 +2,11 @@ package Arnaud;
 
 
 import Model.Database;
-import Arnaud.JoueurDAO;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.swing.Timer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,15 +20,17 @@ import java.util.logging.Logger;
  *
  * @author Arnaud
  */
-public class Lobby extends javax.swing.JFrame {
+public class Lobby extends javax.swing.JFrame implements ActionListener {
 
     private JoueurDAO joueur;
-    private Boolean attaquantRougeSelectionne;
-    private Boolean attaquantBleuSelectionne ;
-    private Boolean defenseurRougeSelectionne ;
-    private Boolean defenseurBleuSelectionne ;
+    private Boolean attaquantRougeSelectionne=false;
+    private Boolean attaquantBleuSelectionne=false ;
+    private Boolean defenseurRougeSelectionne=false ;
+    private Boolean defenseurBleuSelectionne=false ;
     private Database bdd;
     private ResultSet res;
+    private Timer timer;
+    private static final int delay = 500;
     /**
      * Get the value of joueur
      *
@@ -49,31 +53,108 @@ public class Lobby extends javax.swing.JFrame {
      * Creates new form Lobby
      */
     public Lobby() {
+        initComponents();
+        raffraichir();
+        timer= new Timer(delay,this);
+        timer.start();
+    }
+    
+    public void raffraichir(){
         try{        
-            this.bdd=new Database();
-            bdd.connect();
-            res = bdd.executeQuery("SELECT Selectionné FROM partie WHERE Rôle='AttaquantRouge'");
-            while (res.next()) {
-                attaquantRougeSelectionne = res.getBoolean(1);
+                this.bdd=new Database();
+                bdd.connect();
+                res = bdd.executeQuery("SELECT Selectionné FROM partie WHERE Rôle='AttaquantRouge'");
+                while (res.next()) {
+                    attaquantRougeSelectionne = res.getBoolean(1);
+                }
+                res = bdd.executeQuery("SELECT Selectionné FROM partie WHERE Rôle='AttaquantBleu'");
+                while (res.next()) {
+                    attaquantBleuSelectionne = res.getBoolean(1);
+                }
+                res = bdd.executeQuery("SELECT Selectionné FROM partie WHERE Rôle='DefenseurRouge'");
+                while (res.next()) {
+                    defenseurRougeSelectionne = res.getBoolean(1);
+                }
+                res = bdd.executeQuery("SELECT Selectionné FROM partie WHERE Rôle='DefenseurBleu'");
+                while (res.next()) {
+                    defenseurBleuSelectionne = res.getBoolean(1);
+                }
             }
-            res = bdd.executeQuery("SELECT Selectionné FROM partie WHERE Rôle='AttaquantBleu'");
-            while (res.next()) {
-                attaquantBleuSelectionne = res.getBoolean(1);
+            catch (SQLException ex) {
+                Logger.getLogger(Lobby.class.getName()).log(Level.SEVERE, null, ex);
             }
-            res = bdd.executeQuery("SELECT Selectionné FROM partie WHERE Rôle='DefenseurRouge'");
-            while (res.next()) {
-                defenseurRougeSelectionne = res.getBoolean(1);
+        jCheckBox1.setSelected(attaquantRougeSelectionne);
+        jCheckBox2.setSelected(attaquantBleuSelectionne);
+        jCheckBox3.setSelected(defenseurRougeSelectionne);
+        jCheckBox4.setSelected(defenseurBleuSelectionne);
+        jCheckBox1.setEnabled(!attaquantRougeSelectionne);
+        jCheckBox2.setEnabled(!attaquantBleuSelectionne);
+        jCheckBox3.setEnabled(!defenseurRougeSelectionne);
+        jCheckBox2.setEnabled(!defenseurBleuSelectionne);
+        String pseudo1 = "";
+        String pseudo2 = "";
+        String pseudo3 = "";
+        String pseudo4 = "";
+        try {
+            if (jCheckBox1.isSelected()){
+                res = bdd.executeQuery("SELECT pseudo FROM joueur WHERE joueur.IdJoueur=(SELECT IdJoueur FROM partie WHERE Rôle='AttaquantRouge')");
+                while (res.next()){
+                    pseudo1=res.getString(1);
+                }
             }
-            res = bdd.executeQuery("SELECT Selectionné FROM partie WHERE Rôle='DefenseurBleu'");
-            while (res.next()) {
-                defenseurBleuSelectionne = res.getBoolean(1);
+            else{
+                pseudo1="Vide";
             }
         }
         catch (SQLException ex) {
-            Logger.getLogger(Lobby.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(jLabel1.getClass().getName()).log(Level.SEVERE, null, ex);
         }
-        initComponents();
-        
+        jLabel1.setText(pseudo1);
+        try {
+            if (jCheckBox2.isSelected()){
+                res = bdd.executeQuery("SELECT pseudo FROM joueur WHERE joueur.IdJoueur=(SELECT IdJoueur FROM partie WHERE Rôle='AttaquantBleu')");
+                while (res.next()){
+                    pseudo2=res.getString(1);
+                }
+            }
+            else{
+                pseudo2="Vide";
+            }
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(jLabel2.getClass().getName()).log(Level.SEVERE, null, ex);
+        }
+        jLabel2.setText(pseudo2);
+        try {
+            if (jCheckBox3.isSelected()){
+                res = bdd.executeQuery("SELECT pseudo FROM joueur WHERE joueur.IdJoueur=(SELECT IdJoueur FROM partie WHERE Rôle='DefenseurRouge')");
+                while (res.next()){
+                    pseudo3=res.getString(1);
+                }
+            }
+            else{
+                pseudo3="Vide";
+            }
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(jLabel3.getClass().getName()).log(Level.SEVERE, null, ex);
+        }
+        jLabel3.setText(pseudo3);
+        try {
+            if (jCheckBox4.isSelected()){
+                res = bdd.executeQuery("SELECT pseudo FROM joueur WHERE joueur.IdJoueur=(SELECT IdJoueur FROM partie WHERE Rôle='DefenseurBleu')");
+                while (res.next()){
+                    pseudo4=res.getString(1);
+                }
+            }
+            else{
+                pseudo4="Vide";
+            }
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(jLabel4.getClass().getName()).log(Level.SEVERE, null, ex);
+        }
+        jLabel4.setText(pseudo4);
     }
 
     /**
@@ -101,6 +182,9 @@ public class Lobby extends javax.swing.JFrame {
             }
             public void windowClosing(java.awt.event.WindowEvent evt) {
                 formWindowClosing(evt);
+            }
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
             }
         });
 
@@ -144,7 +228,7 @@ public class Lobby extends javax.swing.JFrame {
         String text1 = "";
         try {
             if (jCheckBox1.isSelected()){
-                res = bdd.executeQuery("SELECT pseudo FROM joueur WHERE joueur.IdJoueur=(SELECT IdJoueur FROM partie WHERE Rôle='DefenseurRouge')");
+                res = bdd.executeQuery("SELECT pseudo FROM joueur WHERE joueur.IdJoueur=(SELECT IdJoueur FROM partie WHERE Rôle='AttaquantRouge')");
                 while (res.next()){
                     text1=res.getString(1);
                 }
@@ -162,7 +246,7 @@ public class Lobby extends javax.swing.JFrame {
         String text2 = "";
         try {
             if (jCheckBox2.isSelected()){
-                res = bdd.executeQuery("SELECT pseudo FROM joueur WHERE joueur.IdJoueur=(SELECT IdJoueur FROM partie WHERE Rôle='DefenseurRouge')");
+                res = bdd.executeQuery("SELECT pseudo FROM joueur WHERE joueur.IdJoueur=(SELECT IdJoueur FROM partie WHERE Rôle='AttaquantBleu')");
                 while (res.next()){
                     text2=res.getString(1);
                 }
@@ -198,7 +282,7 @@ public class Lobby extends javax.swing.JFrame {
         String text4 = "";
         try {
             if (jCheckBox4.isSelected()){
-                res = bdd.executeQuery("SELECT pseudo FROM joueur WHERE joueur.IdJoueur=(SELECT IdJoueur FROM partie WHERE Rôle='DefenseurRouge')");
+                res = bdd.executeQuery("SELECT pseudo FROM joueur WHERE joueur.IdJoueur=(SELECT IdJoueur FROM partie WHERE Rôle='DefenseurBleu')");
                 while (res.next()){
                     text4=res.getString(1);
                 }
@@ -261,9 +345,9 @@ public class Lobby extends javax.swing.JFrame {
                 .addContainerGap(209, Short.MAX_VALUE))
         );
 
-        jCheckBox1.setEnabled(!jCheckBox1.isSelected());
+        jCheckBox1.setEnabled(!attaquantRougeSelectionne);
         jCheckBox2.setEnabled(!jCheckBox2.isSelected());
-        jCheckBox3.setEnabled(!jCheckBox3.isSelected());
+        jCheckBox3.setEnabled(!defenseurRougeSelectionne);
         jCheckBox2.setEnabled(!jCheckBox2.isSelected());
 
         pack();
@@ -271,19 +355,46 @@ public class Lobby extends javax.swing.JFrame {
 
     private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
         // TODO add your handling code here:
-        
+        bdd.executeQuery("UPDATE partie SET IdJoueur="+joueur.getId()+",Selectionné=1 WHERE Rôle='AttaquantRouge'");
+        attaquantRougeSelectionne=true;
+        jLabel1.setText(joueur.getPseudo());
+        jCheckBox1.setEnabled(false);
+        jCheckBox2.setEnabled(false);
+        jCheckBox3.setEnabled(false);
+        jCheckBox4.setEnabled(false);
     }//GEN-LAST:event_jCheckBox1ActionPerformed
 
     private void jCheckBox2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox2ActionPerformed
         // TODO add your handling code here:
+        bdd.executeQuery("UPDATE partie SET IdJoueur="+joueur.getId()+",Selectionné=1 WHERE Rôle='AttaquantBleu'");
+        attaquantBleuSelectionne=true;
+        jLabel2.setText(joueur.getPseudo());
+        jCheckBox1.setEnabled(false);
+        jCheckBox2.setEnabled(false);
+        jCheckBox3.setEnabled(false);
+        jCheckBox4.setEnabled(false);
     }//GEN-LAST:event_jCheckBox2ActionPerformed
 
     private void jCheckBox3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox3ActionPerformed
         // TODO add your handling code here:
+        bdd.executeQuery("UPDATE partie SET IdJoueur="+joueur.getId()+",Selectionné=1 WHERE Rôle='DefenseurRouge'");
+        defenseurRougeSelectionne=true;
+        jLabel3.setText(joueur.getPseudo());
+        jCheckBox1.setEnabled(false);
+        jCheckBox2.setEnabled(false);
+        jCheckBox3.setEnabled(false);
+        jCheckBox4.setEnabled(false);
     }//GEN-LAST:event_jCheckBox3ActionPerformed
 
     private void jCheckBox4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox4ActionPerformed
         // TODO add your handling code here:
+        bdd.executeQuery("UPDATE partie SET IdJoueur="+joueur.getId()+",Selectionné=1 WHERE Rôle='DefenseurBleu'");
+        defenseurBleuSelectionne=true;
+        jLabel4.setText(joueur.getPseudo());
+        jCheckBox1.setEnabled(false);
+        jCheckBox2.setEnabled(false);
+        jCheckBox3.setEnabled(false);
+        jCheckBox4.setEnabled(false);
     }//GEN-LAST:event_jCheckBox4ActionPerformed
 
     private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
@@ -292,9 +403,17 @@ public class Lobby extends javax.swing.JFrame {
     }//GEN-LAST:event_formWindowClosing
 
     private void formWindowActivated(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowActivated
-        // TODO add your handling code here:try {    
+        // TODO add your handling code here:try { 
     }//GEN-LAST:event_formWindowActivated
 
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+        // TODO add your handling code here:        
+    }//GEN-LAST:event_formWindowOpened
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        raffraichir();
+    }
     /**
      * @param args the command line arguments
      */
