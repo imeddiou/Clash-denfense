f402319cd74051a9a34a94d44d52fb2564f9021a
silvereens2m <silvere.bardin@ens2m.org>
2020-04-30 07:53:17 +0200

    affichage de la carte avec placement des tours

---
+++ ClashDefense/src/Silvere/Affichage_map.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Silvere;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+/**
+ *
+ * @author Silvère BARDIN
+ */
+public class Affichage_map {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+
+            Connection connexion = DriverManager.getConnection("jdbc:mysql://nemrod.ens2m.fr:3306/20192020_s2_vs1_tp1_clashdefense?serverTimezone=UTC", "clashdefense", "WCvYk10DhJUNKsdX");
+
+            PreparedStatement requete = connexion.prepareStatement("SELECT Idtour, PositionX, PositionY FROM tour;");
+            ResultSet resultat = requete.executeQuery();
+            ArrayList Id = new ArrayList();
+            ArrayList PosX = new ArrayList();
+            ArrayList PosY = new ArrayList();
+//            ArrayList Eq = new ArrayList();
+            int[][] map =new int[20][];
+            for (int i=0 ; i<map.length; i=i+1){
+                map[i]=new int[20];             // définition d'une map de taille 20x20
+            }
+            for (int i=0 ; i<map.length; i=i+1){
+                for (int j=0 ; j<map[i].length; j=j+1){
+                    map[i][j]=0;
+                }
+            }
+            for (int i=0 ; i<map.length; i=i+1){
+                for (int j=4 ; j<8; j=j+1){
+                    map[i][j]=-1;                   // définition d'un premier chemin
+                }
+            }
+            for (int i=0 ; i<map.length; i=i+1){
+                for (int j=12 ; j<16; j=j+1){
+                    map[i][j]=-1;                   // définition d'un deuxième chemin
+                }
+            }
+           
+            while (resultat.next()) {
+                int Idtour = resultat.getInt("Idtour");
+                Id.add(Idtour);
+                int PositionX = resultat.getInt("PositionX");
+                PosX.add(PositionX);
+                int PositionY = resultat.getInt("PositionY");
+                PosY.add(PositionY);
+//                String Equipe = resultat.getString("Equipe");
+//                Eq.add(Equipe);
+                //double latitude = resultat.getDouble("latitude");
+                //double longitude = resultat.getDouble("longitude");
+                //System.out.println(pseudo + " = (" + latitude + "; " + longitude + ")");
+                
+                //System.out.println(Idtour + " à la position X "+ PositionX + " et la position Y " + PositionY);
+//                ArrayList Id = new ArrayList();
+//                ArrayList PosX = new ArrayList();
+//                ArrayList PosY = new ArrayList();
+
+            
+            }
+            requete.close();
+            connexion.close();
+        for(int i=0;i<Id.size();i=i+1){
+            map[(int)PosX.get(i)][(int)PosY.get(i)]=(int)Id.get(i) ;    // On place les tours à leurs positions
+        
+        }
+        JFrame fenetre = new JFrame();                                  //création de la map
+        JPanel pan = new JPanel (new GridLayout (20,20));
+        Border blackline = BorderFactory.createLineBorder(Color.black,1);
+        for(int i = 0; i<400;i++){
+            int Q = i/20;
+            int R  = i%20;
+            JPanel Case = new JPanel();
+            Case.setBorder(blackline);
+            if(map[Q][R]==0){                              // zone de construction des tours
+                Color Brown = new Color(51,0,0); 
+                Case.setBackground(Brown);
+            }else if(map[Q][R]==-1){                        // chemins
+                Color Sable = new Color(255,204,102);
+                Case.setBackground(Sable);
+            }else if(map[Q][R]>0){                          // affichage des tours
+//                int k = Id.indexOf(i);
+//                if(Eq.get(k).equals("Bleue")){            //changer la couleur des tours en fonction de leur équipe
+//                    Case.setBackground(Color.BLUE);
+//                }else{
+//                    Case.setBackground(Color.red);
+//                }
+                 Case.setBackground(Color.GRAY);
+            }
+            pan.add(Case);
+        }
+        pan.setBorder(blackline);
+        fenetre.add(pan);
+        fenetre.setVisible(true);
+        fenetre.setSize(850,650);
+//        for (int i = 0; i<map.length; i=i+1){
+//            for (int j = 0; j<map[j].length; j=j+1)
+//            System.out.println(map[i][j]);
+//     }
+//        for (int i = 0; i<map.length; i=i+1){
+//            System.out.println(map[i]);
+//        }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+   
+    }
+}
