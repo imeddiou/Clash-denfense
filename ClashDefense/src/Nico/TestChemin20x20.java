618170f0308f923083a0ead57f7bab5de2f38ef9
NicoAAAA <63840275+NicoAAAA@users.noreply.github.com>
2020-06-06 01:22:23 +0200

    Monstre suiveur de chemin Tour attaquante Creation de la map

--- ClashDefense/src/Nico/TestChemin20x20.java
+++ ClashDefense/src/Nico/TestChemin20x20.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 
 public class TestChemin20x20 {
     public static void main(String[] args){
-        Chemin20x20 chemin = new Chemin20x20();
-        ArrayList<ArrayList<Integer>> map = chemin.CreationMap();
-        for(int x=0;x<map.size();x++){
+        Chemin20x20 chemin = new Chemin20x20();//On créé un nouveau chemin
+        ArrayList<ArrayList<Integer>> map = chemin.CreationMap();//on récupère la carte
+        for(int x=0;x<map.size();x++){//on balaye la map
             for(int y=0;y<map.get(0).size();y++){
-                if (map.get(x).get(y)!=0){
-                    System.out.print(-map.get(x).get(y));}
-                else{System.out.print(".");}
+                if (map.get(x).get(y)!=0){//Si la coordonnée est celle d'un chemin:
+                    System.out.print(-map.get(x).get(y));}//On affiche la constante qui caractérise le chemin
+                else{System.out.print(".");}//Sinon on affiche de . pour une question de visibilité
             }
             System.out.println();
         }
