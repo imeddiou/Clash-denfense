54ae7fa644b24db3652140494dc450917a348e9b
Fanny772 <fanny.pimont@ens2m.org>
2020-06-11 16:12:13 +0200

    Mise à jour

--- ClashDefense/src/Fanny/MonstreRequête.java
+++ ClashDefense/src/Fanny/MonstreRequête.java
@@ -21,21 +21,33 @@ public class MonstreRequête {
     }
     
     public void monstreRequêteInsertion(Database baseDeDonnées,String description, String couleur){          
-        try {            
-            ResultSet resultat0 = baseDeDonnées.executeQuery("SELECT MAX(IdMonstre) FROM monstre WHERE Description = '"+description+"' AND Equipe = '"+couleur+"'");        
-            int dernierIdMonstre = 0; 
+        try {             
+            int dernierIdMonstre = 0;
+            int idMax= 0;
+            ResultSet resultat0 = baseDeDonnées.executeQuery("SELECT MAX(IdMonstre) FROM monstre WHERE Description = '"+description+"' AND Equipe = '"+couleur+"'");
+            while (resultat0.next()){
+                idMax = resultat0.getInt(1);
+                if (idMax == 0){
+                    ResultSet resultat1 = baseDeDonnées.executeQuery("SELECT IdMonstre FROM catalogueMonstre WHERE Description = '"+description+"' AND Equipe = '"+couleur+"'");
+                    while (resultat1.next()){
+                        dernierIdMonstre = resultat1.getInt(1)+1;
+                    }
+                }  
+                else{
+                    ResultSet resultat2 = baseDeDonnées.executeQuery("SELECT MAX(IdMonstre) FROM monstre WHERE Description = '"+description+"' AND Equipe = '"+couleur+"'");
+                    while (resultat2.next()){
+                        dernierIdMonstre = resultat2.getInt(1)+1;
+                    }
+                }                
+            }           
             double positionX = 0.0;
             double positionY = 0.0;
             double vitesse = 0.0;
             int pdV = 0;
             int direction = 0;
             int avancée = 0;
-            while (resultat0.next()){
-                dernierIdMonstre = resultat0.getInt(1)+1;                
-            }
             ResultSet resultat1 = baseDeDonnées.executeQuery("SELECT * FROM cataloguemonstre WHERE Description = '"+description+"' AND Equipe = '"+couleur+"'");
             while (resultat1.next()){
-                dernierIdMonstre = dernierIdMonstre + resultat1.getInt(1);
                 positionX = resultat1.getDouble("PositionX");
                 positionY = resultat1.getDouble("PositionY");
                 vitesse = resultat1.getDouble("Vitesse");
@@ -52,6 +64,18 @@ public class MonstreRequête {
     
     
     public void monstreRequêteModificationPosition (Database baseDeDonnées,int idMonstre, double positionX, double positionY){  
-           
+           try {  
+            ResultSet resultat0 = baseDeDonnées.executeQuery("SELECT PositionX FROM monstre WHERE idMonstre = '"+idMonstre+"' ");        
+            while (resultat0.next()){
+                positionX = resultat0.getDouble("PositionX");                
+            }
+            ResultSet resultat1 = baseDeDonnées.executeQuery("SELECT PositionY FROM monstre WHERE idMonstre = '"+idMonstre+"' ");
+            while (resultat1.next()){
+                positionY = resultat1.getDouble("PositionY"); 
+            }                
+            baseDeDonnées.executeQuery("UPDATE monstre SET PositionX = '"+positionX+"', PositionY = '"+positionY+"' WHERE idMonstre = '"+idMonstre+"' ");
+        } catch (SQLException ex) {
+            Logger.getLogger(JoueurRequête.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }            
 }
