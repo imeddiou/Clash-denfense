db11bd6792c3ddaf887f9496c4e797b1b638f2d3
Ibrahim Meddioui <ibrahim.meddioui@ens2m.org>
2020-06-12 19:16:44 +0200

    Rassemeblement du code 11

--- ClashDefense/src/Model/PartieRequête.java
+++ ClashDefense/src/Model/PartieRequête.java
@@ -9,6 +9,8 @@ import Fanny.*;
 import Ibrahim.Database;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,57 +19,73 @@ import java.util.logging.Logger;
  * @author fanny
  */
 public class PartieRequête {
-        private Database baseDeDonnées;
-
+    
     public PartieRequête(){
-           try {
-            baseDeDonnées.connect();
-        } catch (SQLException ex) {
-            Logger.getLogger(MonstreRequête.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        
     }
     
-    public void partieRequêteModificationIdJoueur(int idJoueur){
-        try {
-            ResultSet resultat0 = baseDeDonnées.executeQuery("SELECT Rôle FROM partie WHERE IdJoueur = '"+idJoueur+"'");
-            String Rôle = "";
-            while (resultat0.next()){
-                Rôle = resultat0.getString("Rôle");
-            }
-                if (Rôle.contains("Bleu")){
-                    if (Rôle.contains("Attaquant")){
-                        baseDeDonnées.executeQuery("UPDATE équipe SET IdJoueurAttaquant = '"+idJoueur+"' WHERE Couleur = 'Bleue' ");                    
-                    }
-                    else{
-                        baseDeDonnées.executeQuery("UPDATE équipe SET IdJoueurDéfenseur = '"+idJoueur+"' WHERE Couleur = 'Bleue' ");                    
-                    }
-                }
-                else {
-                    if (Rôle.contains("Attaquant")){
-                        baseDeDonnées.executeQuery("UPDATE équipe SET IdJoueurAttaquant = '"+idJoueur+"' WHERE Couleur = 'Rouge' ");                    
-                    }
-                    else{
-                        baseDeDonnées.executeQuery("UPDATE équipe SET IdJoueurDéfenseur = '"+idJoueur+"' WHERE Couleur = 'Rouge' ");                    
-                    }
-                }
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(JoueurRequête.class.getName()).log(Level.SEVERE, null, ex);
-        }          
+    public void ModificationIdJoueur(Database baseDeDonnées,int idJoueur, String rôle){
+        baseDeDonnées.executeQuery("UPDATE partie SET IdJoueur = '"+idJoueur+"' WHERE Rôle = '"+rôle+"' ");        
     }
     
     
-    public void partieRequêteModificationPosition (String rôle, double positionX, double positionY){
+    public void ModificationPosition (Database baseDeDonnées,String rôle, double positionX, double positionY){
         try {
-            ResultSet resultat0 = baseDeDonnées.executeQuery("SELECT selectionné FROM partie WHERE Rôle = 'rôle'");
+            ResultSet resultat0 = baseDeDonnées.executeQuery("SELECT Selectionné FROM partie WHERE Rôle = '"+rôle+"'");
             if (resultat0.getBoolean("Selectionné")){
-                baseDeDonnées.executeQuery("UPDATE partie SET PositionX = 'positionX' WHERE Rôle = 'rôle' ");
-                baseDeDonnées.executeQuery("UPDATE partie SET PositionY = 'positionY' WHERE Rôle = 'rôle' ");
-                ResultSet resultat = baseDeDonnées.executeQuery("SELECT * FROM partie");
+                baseDeDonnées.executeQuery("UPDATE partie SET PositionX = '"+positionX+"' WHERE Rôle = '"+rôle+"' ");
+                baseDeDonnées.executeQuery("UPDATE partie SET PositionY = '"+positionY+"' WHERE Rôle = '"+rôle+"' ");
             }
         } catch (SQLException ex) {
             Logger.getLogger(JoueurRequête.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    public void partieRequêteStockageMap(Database baseDeDonnées, ArrayList<ArrayList<Integer>> map){
+        String mapString = "";
+        for(int i=0; i< map.size();i++){
+            for(int k=0;k<map.get(0).size();k++){
+                 mapString += map.get(i).get(k)+ " ";
+            }
+        }
+       
+        
+        System.out.println(mapString);
+        baseDeDonnées.executeQuery("UPDATE partie SET Map = '" + mapString + "'"  );
+    }
+    public ArrayList<ArrayList<Integer>> partieRequêteSelectMapAsMatrix(Database db){
+       ResultSet rs =  db.executeQuery("SELECT DISTINCT Map FROM partie");
+      ArrayList<ArrayList<Integer>> mapFinal = new ArrayList<ArrayList<Integer>>();
+       String mapString = "";
+        try {
+            while(rs.next()){
+                mapString = rs.getString("Map");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PartieRequête.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String[] mapStringSplitted = mapString.split(" ");
+         for (int i=0;i<20;i++){
+            ArrayList<Integer> ligneMap = new ArrayList<Integer>();
+            for (int j=0;j<20;j++){
+                ligneMap.add(0);}
+            mapFinal.add(ligneMap);
+         }
+        System.out.println(mapStringSplitted.length);
+        for(int i=0;i<mapStringSplitted.length;i++){
+            for(int j=0;j<20;j++){
+                for(int k=0; k<20; k++){
+                    int nbr = Integer.parseInt(mapStringSplitted[i]);
+                 mapFinal.get(j).set(k,nbr);
+                }
+              }
+            }
+             System.out.println(mapFinal);
+
+        
+      
+      
+        
+        return mapFinal;
     }
     
 }
