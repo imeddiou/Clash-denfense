9d7f0247a7156600de481d492735ca7fa8119461
ArnaudRolin <63839312+ArnaudRolin@users.noreply.github.com>
2020-06-19 20:24:03 +0200

    map

--- ClashDefense/src/Archive_des_classes/TestChemin20x20.java
+++ ClashDefense/src/Archive_des_classes/TestChemin20x20.java
@@ -10,10 +10,10 @@ public class TestChemin20x20 {
         for(int x=0;x<map.size();x++){//on balaye la map
             for(int y=0;y<map.get(0).size();y++){
                 if (map.get(x).get(y)!=0){//Si la coordonnée est celle d'un chemin:
-                    System.out.print(map.get(x).get(y));}//On affiche la constante qui caractérise le chemin
-                else{System.out.print("0");}//Sinon on affiche de . pour une question de visibilité
+                    System.out.print(map.get(x).get(y)+" ");}//On affiche la constante qui caractérise le chemin
+                else{System.out.print("0 ");}//Sinon on affiche de . pour une question de visibilité
             }
-            System.out.println();
+            System.out.print("");
         }
     }
 }
